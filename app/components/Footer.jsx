4dc0c572aa583a886bc8856b7847f06bdaf5929a
Gabriel Marchegiani <gabrielmarchegiani@iMac-di-Gabriel.local>
2024-01-26 17:28:07 +0100

    reversed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,14 +9,12 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
-import iOSInstructionsModal from '../components/iOSInstructionModal'; // Assicurati che il percorso sia corretto
 
 const Footer = () => {
   const router = useRouter();
   const [installPrompt, setInstallPrompt] = useState(null);
   const [isIOS, setIsIOS] = useState(false);
   const [isPWAInstalled, setIsPWAInstalled] = useState(false);
-  const [showiOSInstructions, setShowiOSInstructions] = useState(false);
 
   useEffect(() => {
     // Controlla se l'app è in modalità standalone (cioè se è stata installata)
@@ -42,8 +40,8 @@ const Footer = () => {
 
   const handleInstallClick = () => {
     if (isIOS) {
-      // Mostra il modale con le istruzioni per iOS
-      setShowiOSInstructions(true);
+      // Mostra le istruzioni per aggiungere la PWA alla schermata Home su iOS
+      alert("Per installare questa app, tocca l'icona di condivisione e poi 'Aggiungi a schermata Home'.");
     } else {
       // Se c'è un evento di installazione salvato e non siamo su iOS, mostralo
       if (installPrompt) {
@@ -109,10 +107,6 @@ const Footer = () => {
           />
         )}
       </BottomNavigation>
-      <iOSInstructionsModal 
-        open={showiOSInstructions} 
-        handleClose={() => setShowiOSInstructions(false)} 
-      />
     </footer>
   );
 };
