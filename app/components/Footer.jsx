586f154b4fdf2900d9cf63b52cdd9e04462d092c
Gabriel Marchegiani <gabrielmarchegiani@iMac-di-Gabriel.local>
2024-01-26 17:19:14 +0100

    added responsive pwa footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -14,31 +14,46 @@ const Footer = () => {
   const router = useRouter();
   const [installPrompt, setInstallPrompt] = useState(null);
   const [isIOS, setIsIOS] = useState(false);
+  const [isPWAInstalled, setIsPWAInstalled] = useState(false);
 
   useEffect(() => {
+    // Controlla se l'app è in modalità standalone (cioè se è stata installata)
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches ||
+                         (window.navigator.standalone) || document.referrer.includes('android-app://');
+    setIsPWAInstalled(isStandalone);
+
+    // Controllo per dispositivi iOS
     setIsIOS(
       /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
     );
 
     // Ascolta l'evento beforeinstallprompt
     window.addEventListener('beforeinstallprompt', (e) => {
+      // Impedisci al browser di mostrare il prompt di installazione
       e.preventDefault();
-      setInstallPrompt(e);
+      // Salva l'evento per poterlo attivare più tardi solo se l'app non è già installata
+      if (!isStandalone) {
+        setInstallPrompt(e);
+      }
     });
   }, []);
 
   const handleInstallClick = () => {
     if (isIOS) {
+      // Mostra le istruzioni per aggiungere la PWA alla schermata Home su iOS
       alert("Per installare questa app, tocca l'icona di condivisione e poi 'Aggiungi a schermata Home'.");
     } else {
+      // Se c'è un evento di installazione salvato e non siamo su iOS, mostralo
       if (installPrompt) {
         installPrompt.prompt();
         installPrompt.userChoice.then((choiceResult) => {
           if (choiceResult.outcome === 'accepted') {
             console.log('L\'utente ha accettato l\'installazione della PWA');
+            setIsPWAInstalled(true); // Aggiorna lo stato una volta che l'app è installata
           } else {
             console.log('L\'utente ha rifiutato l\'installazione della PWA');
           }
+          // Dopo aver gestito l'evento, imposta installPrompt a null
           setInstallPrompt(null);
         });
       }
