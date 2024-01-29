3abae0c175b15ea7a96002f5f300747842a1a2be
Gabriel Marchegiani <gabrielmarchegiani@iMac-di-Gabriel.local>
2024-01-29 18:13:55 +0100

    fix: bugs

--- app/esperienze/page.jsx
+++ app/esperienze/page.jsx
@@ -13,8 +13,8 @@ const esperienze = [
     imageUrl:
       "https://images.pexels.com/photos/1277181/pexels-photo-1277181.jpeg?auto=compress&cs=tinysrgb&w=600",
     buttonText: "Prenota",
-    bookUrl: '/esperienze/prenota',
-    location: 'Riccione',
+    bookUrl: "/esperienze/prenota",
+    location: "Riccione",
     duration: 60,
     cost: 60,
   },
@@ -26,10 +26,10 @@ const esperienze = [
     imageUrl:
       "https://images.pexels.com/photos/1277181/pexels-photo-1277181.jpeg?auto=compress&cs=tinysrgb&w=600",
     buttonText: "Prenota",
-    bookUrl: '/esperienze/prenota',
-    location: 'Riccione',
+    bookUrl: "/esperienze/prenota",
+    location: "Riccione",
     duration: 60,
-    cost: 60
+    cost: 60,
   },
   {
     id: 3,
@@ -39,10 +39,10 @@ const esperienze = [
     imageUrl:
       "https://images.pexels.com/photos/1277181/pexels-photo-1277181.jpeg?auto=compress&cs=tinysrgb&w=600",
     buttonText: "Prenota",
-    bookUrl: '',
-    location: 'Riccione',
+    bookUrl: "",
+    location: "Riccione",
     duration: 60,
-    cost: 60
+    cost: 60,
   },
   {
     id: 4,
@@ -52,42 +52,58 @@ const esperienze = [
     imageUrl:
       "https://images.pexels.com/photos/1277181/pexels-photo-1277181.jpeg?auto=compress&cs=tinysrgb&w=600",
     buttonText: "Prenota",
-    bookUrl: '',
-    location: 'Riccione',
+    bookUrl: "",
+    location: "Riccione",
     duration: 60,
-    cost: 60
+    cost: 60,
   },
 ];
 
 const Esperienze = () => {
   const truncateDescription = (desc) => {
-    return desc.length > 100 ? desc.substring(0, 100) + '...' : desc;
-  }
+    return desc.length > 100 ? desc.substring(0, 100) + "..." : desc;
+  };
 
   return (
-    <div className="h-full">
+    <div className="flex flex-col min-h-screen bg-gray-100">
       <NavigationHeader />
-      <div className="w-full p-6">
-        {/* <ExperienceCard
-          title="Assaggi in vigna"
-          description="Questo è un testo di descrizione della card per le prenotazioni"
-          imageUrl="https://images.pexels.com/photos/1277181/pexels-photo-1277181.jpeg?auto=compress&cs=tinysrgb&w=600"
-          buttonText="Prenota"
-        /> */}
+      <div className="flex-grow">
+
+      <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {esperienze.map((esperienza) => (
-          <div className="w-full p-6" key={esperienza.id}>
-            <Link href={esperienza.bookUrl}>
-            <ExperienceCard
-              title={esperienza.title.toUpperCase()}
-              description={truncateDescription(esperienza.description)}
-              imageUrl={esperienza.imageUrl}
-              buttonText={esperienza.buttonText}
-            />
-            </Link>
+          <div
+            className="flex flex-col bg-white rounded-lg shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg"
+            key={esperienza.id}
+          >
+            <div className="flex-shrink-0">
+              <img
+                src={esperienza.imageUrl}
+                alt={esperienza.title}
+                className="w-full h-48 object-cover rounded-t-lg"
+              />
+            </div>
+            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+              <h3 className="text-gray-900 text-xl font-semibold">
+                {esperienza.title}
+              </h3>
+              <p className="text-gray-600 text-sm flex-1">
+                {truncateDescription(esperienza.description)}
+              </p>
+              <div className="mt-6">
+                <Link legacyBehavior href={esperienza.bookUrl}>
+                  <a className="text-indigo-600 hover:text-indigo-900 font-semibold text-sm transition-colors duration-300">
+                    {esperienza.buttonText}
+                  </a>
+                </Link>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      <Footer />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
