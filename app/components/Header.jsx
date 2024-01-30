f76807c3b0f893a34f93d792d1848066c7e24414
Gabriel Marchegiani <gabrielmarchegiani@iMac-di-Gabriel.local>
2024-01-30 18:06:25 +0100

    added transition to img

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import HamburgerMenu from "./HamburgerMenu";
+import '../slideshow.css'
 
 const Header = () => {
   const images = [
@@ -21,7 +22,6 @@ const Header = () => {
 
     return () => clearInterval(intervalId);
   }, []);
-
   return (
     <div className="relative h-[30vh]">
       <TransitionGroup>
@@ -29,12 +29,14 @@ const Header = () => {
           key={images[currentImageIndex]}
           timeout={500}
           classNames={{
-            enter: 'transition ease-in-out duration-500 transform slide-in-right', 
-            exit: 'transition ease-in-out duration-500 transform slide-out-left'
+            enter: 'fade-enter',
+            enterActive: 'fade-enter-active',
+            exit: 'fade-exit',
+            exitActive: 'fade-exit-active'
           }}
         >
           <div
-            className="absolute inset-0 bg-cover bg-center bg-no-repeat transition-all duration-500"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat transition-opacity duration-500"
             style={{
               backgroundImage: `url(${images[currentImageIndex]})`,
             }}
@@ -42,26 +44,27 @@ const Header = () => {
         </CSSTransition>
       </TransitionGroup>
 
-      <div className="absolute inset-0 bg-black opacity-30"></div>
+      {/* Contenuto del header (incluso il logo) sopra il background */}
+      <div className="absolute inset-0 z-10">
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2"
+          style={{ top: "75%" }}
+        >
+          <img
+            src="https://img.freepik.com/free-vector/wine-logo-template_1195-35.jpg?w=1380&t=st=1705658673~exp=1705659273~hmac=a33f9f1a07c8b9750f6575fb3284c4a3b554fa9a49eb9ad3151e9c9cee30ce37"
+            alt="Logo"
+            className="w-[120px] h-[120px] rounded-lg z-10"
+          />
+        </div>
 
-      <div
-        className="absolute left-1/2 transform -translate-x-1/2"
-        style={{ top: "75%" }}
-      >
-        <img
-          src="https://img.freepik.com/free-vector/wine-logo-template_1195-35.jpg?w=1380&t=st=1705658673~exp=1705659273~hmac=a33f9f1a07c8b9750f6575fb3284c4a3b554fa9a49eb9ad3151e9c9cee30ce37"
-          alt="Logo"
-          className="w-[120px] h-[120px] rounded-lg"
-        />
-      </div>
-
-      <div className="relative z-10">
-        <nav className="flex justify-between items-center p-4 w-full">
-          <HamburgerMenu />
-          <div></div>
-        </nav>
-        <div className="text-center py-20">
-          <h1 className="text-white text-4xl uppercase tracking-widest"></h1>
+        <div className="relative z-10">
+          <nav className="flex justify-between items-center p-4 w-full">
+            <HamburgerMenu />
+            <div></div>
+          </nav>
+          <div className="text-center py-20">
+            <h1 className="text-white text-4xl uppercase tracking-widest"></h1>
+          </div>
         </div>
       </div>
     </div>
