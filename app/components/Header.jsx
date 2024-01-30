e805af54f17df260568be7f2d4c8e924ec8e5531
Gabriel Marchegiani <gabrielmarchegiani@iMac-di-Gabriel.local>
2024-01-30 17:26:52 +0100

    added header transition

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Header = () => {
+  const images = [
+    "https://images.pexels.com/photos/2339181/pexels-photo-2339181.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/51947/tuscany-grape-field-nature-51947.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/943700/pexels-photo-943700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex(
+        (currentImageIndex) => (currentImageIndex + 1) % images.length
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div
-      className="relative bg-cover bg-center bg-no-repeat h-[30vh]"
-      style={{
-        backgroundImage: `url(https://images.pexels.com/photos/2339181/pexels-photo-2339181.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
-      }}
-    >
+    <div className="relative h-[30vh]">
+      <TransitionGroup>
+        <CSSTransition
+          key={images[currentImageIndex]}
+          timeout={500}
+          classNames={{
+            enter: 'transition ease-in-out duration-500 transform slide-in-right', 
+            exit: 'transition ease-in-out duration-500 transform slide-out-left'
+          }}
+        >
+          <div
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat transition-all duration-500"
+            style={{
+              backgroundImage: `url(${images[currentImageIndex]})`,
+            }}
+          ></div>
+        </CSSTransition>
+      </TransitionGroup>
+
       <div className="absolute inset-0 bg-black opacity-30"></div>
 
       <div
